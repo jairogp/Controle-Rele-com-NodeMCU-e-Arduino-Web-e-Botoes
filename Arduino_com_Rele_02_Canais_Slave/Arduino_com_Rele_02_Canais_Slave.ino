b3a6d41b1fbabe95dc1d0efdf3e4af10b6ccf5af
jairogp <jairogp@users.noreply.github.com>
2017-06-05 01:31:41 -0300

    Add files via upload

---
+++ Arduino_com_Rele_02_Canais_Slave/Arduino_com_Rele_02_Canais_Slave.ino
@@ -0,0 +1,55 @@
+
+  #define ch1 3
+  #define ch2 4
+  #define botao1 8
+  #define botao2 9
+  int leitura_ch1;
+  int leitura_ch2;
+  int status_ch1 = 1;
+  int status_ch2 = 1;
+   
+  void setup() {
+    Serial.begin(9600);
+    pinMode(ch1, OUTPUT);
+    pinMode(botao1, INPUT);
+    digitalWrite(ch1, HIGH);
+    pinMode(ch2, OUTPUT);
+    pinMode(botao2, INPUT);
+    digitalWrite(ch2, HIGH);  }
+
+  void Controle() {
+     Canal_01:
+     leitura_ch1 = digitalRead(botao1);
+     if (leitura_ch1 != 0)
+     { while(digitalRead(botao1) != 0)
+     delay(100);
+     status_ch1 = !status_ch1;
+     digitalWrite(ch1, status_ch1); }
+     
+     Canal_02:
+     leitura_ch2 = digitalRead(botao2);
+     if (leitura_ch2 != 0)
+     { while(digitalRead(botao2) != 0)
+     delay(100);
+     status_ch2 = !status_ch2;
+     digitalWrite(ch2, status_ch2); }
+     }
+  
+  void loop() {
+    
+  Controle();  
+    
+  char caracter = Serial.read();
+  if(caracter == 'A')
+  {digitalWrite(ch1,LOW);
+  {status_ch1 = 0;}}
+  if(caracter == 'a')
+  {digitalWrite(ch1,HIGH);
+  {status_ch1 = 1;}}
+  if(caracter == 'B')
+  {digitalWrite(ch2,LOW);
+  {status_ch2 = 0;}}
+  if(caracter == 'b')
+  {digitalWrite(ch2,HIGH);
+  {status_ch2 = 1;}}
+  delay(10);  }
---
+++ NodeMCU_com_Rele_02_Canais_Master/NodeMCU_com_Rele_02_Canais_Master.ino
@@ -0,0 +1,88 @@
+
+  #include <ESP8266WiFi.h>
+
+  const char* ssid = "Vitorelo";
+  const char* password = "setembro1309";
+  WiFiServer server(80);
+
+  void setup() {
+
+  Serial.begin(9600);
+  delay(10);
+  
+  conectar();
+  }
+
+  void conectar() {
+  Serial.println();
+  Serial.println();
+  Serial.print("Connectando a ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("Conectado com sucesso na rede.");
+  
+  // Start the server
+  server.begin();
+  Serial.println("IP Obtido: ");
+  Serial.print(WiFi.localIP());
+  }
+
+  void loop() {
+       
+  if (WiFi.status() != WL_CONNECTED) {conectar();}  // Caso wifi tenha caido conecta novamente
+  WiFiClient client = server.available();
+  // Volta ao loop aguardando nova conexao do cliente.
+  if (!client) { 
+    return; 
+  }     
+  // um loop de 20 laços para nao ficar preso quando da erro na conexao client  problemas com o navegador Chrome           
+  int conta = 0 ;
+  while(conta < 20)            
+  {
+  if(!client.available()){ conta = conta +1;}
+  else { conta = 20;}
+  delay(1);
+  } 
+  // mesmo passando o laço e ainda nao teve conexao, para e retorna ao inicio eliminando erros de navegadores
+  if (!client.available()) 
+  { 
+    client.stop();
+    return;
+  } 
+  String req = client.readStringUntil('\r');
+  client.flush();
+  
+  String buf = "";
+ 
+  buf += "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n";
+  buf += "<html lang=\"pt\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>\r\n";
+  buf += "<title>WEB Server ESP 8266</title>";
+  buf += "<h1> CONTROLE ILUMINACAO</h1>";
+  buf += "<style>.c{text-align: center;} div,input{padding:5px;font-size:1em;} input{width:80%;} body{text-align: center;font-family:verdana;} button{border:0;border-radius:0.3rem;background-color:#1fa3ec;color:#fff;line-height:2.4rem;font-size:1.2rem;width:49%;} .q{float: right;width: 64px;text-align: right;}</style>";
+  buf += "<p><h3>CANAL 01</h3></p> <a href=\"?function=D0_on\"><button>LIGA</button></a> <a href=\"?function=D0_off\"><button>DESLIGA</button></a></p>";
+  buf += "<p><h3>CANAL 02</h3></p> <a href=\"?function=D1_on\"><button>LIGA</button></a> <a href=\"?function=D1_off\"><button>DESLIGA</button></a></p>";
+  client.print(buf);
+  delay(10);
+
+  client.flush();
+
+       if (req.indexOf("D0_on") != -1)
+    Serial.println("A");
+       if (req.indexOf("D0_off") != -1)
+    Serial.println("a");
+       if (req.indexOf("D1_on") != -1)
+    Serial.println("B");
+       if (req.indexOf("D1_off") != -1)
+    Serial.println("b");
+  else{
+    client.stop();
+  }}
+
